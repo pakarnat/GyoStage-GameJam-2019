91c99e6e5ddd9d4d14417c5b31fca419829e076e
pakarnat <pakarinen.kimmo@gmail.com>
2019-10-18 20:08:11 +0300

    Add files via upload

---
+++ Assets/Scripts/TableRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableRotation : MonoBehaviour
+{
+
+    Transform table;
+    [SerializeField]
+    GameObject PlayerBall;
+    [SerializeField]
+    float speed = 1.0f;
+    [SerializeField]
+    bool wrongControls = false;
+
+    
+    void Start()
+    {        
+        table = GetComponent<Transform>();        
+    }  
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (wrongControls)
+        {
+
+            table.Rotate(new Vector3(-Input.GetAxis("Mouse X"), 0, -Input.GetAxis("Mouse Y")) * Time.deltaTime * speed);
+        }
+        else
+        {
+            table.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, Input.GetAxis("Mouse X")) * Time.deltaTime * speed);
+        }
+    }
+}
